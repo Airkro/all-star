a30ce074a9f3cf2a0b6f97f1b424402d50e6acf8
Eric <airkro@qq.com>
2024-06-13 14:50:52 +0800

    chore(deps): update

--- syncpack.config.mjs
+++ syncpack.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 
-import defineConfig from '@nice-move/syncpack-config';
+import defineConfig from '@nice-move/syncpack-config/define.cjs';
 
 export default defineConfig(import.meta.url, {
   semverGroups: [
@@ -14,7 +14,7 @@ export default defineConfig(import.meta.url, {
       dependencies: ['@all-star/*'],
       dependencyTypes: ['local'],
       label: 'Same version',
-      pinVersion: '0.16.8',
+      pinVersion: '0.16.9',
     },
   ],
 });
