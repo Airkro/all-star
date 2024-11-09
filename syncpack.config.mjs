113d0a761dfe2c2bd2689a92f0ea446fd19b27cb
Eric Chen <airkro@qq.com>
2024-11-09 16:57:55 +0800

    chore(deps): update

--- syncpack.config.mjs
+++ syncpack.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig(import.meta.url, {
       dependencies: ['@all-star/*'],
       dependencyTypes: ['local'],
       label: 'Same version',
-      pinVersion: '0.17.19',
+      pinVersion: '0.17.20',
     },
   ],
 });
