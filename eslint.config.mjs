bc1a6ec19161ce2143f39b8832ec14c66e198619
Eric <airkro@qq.com>
2025-01-10 16:20:03 +0800

    chore: use eslint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import base from '@nice-move/eslint-config-base';
+
+export default [
+  ...base,
+  {
+    ignores: ['src/**'],
+  },
+];
